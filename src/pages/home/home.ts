06ea08e230bfacf8b95b5cdab5db586a5d0f2c68
Rafael Dantas <rafaeldantasr@hotmail.com>
2018-11-07 12:53:33 -0200

    initial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+ 
+declare var google;
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+    map: any;
+ 
+    constructor(private platform: Platform) {
+ 
+    }
+ 
+    ionViewDidLoad(){
+        this.initializeMap();
+    }
+ 
+    initializeMap() {
+ 
+        let locationOptions = {timeout: 20000, enableHighAccuracy: true};
+ 
+        navigator.geolocation.getCurrentPosition(
+ 
+            (position) => {
+ 
+                let options = {
+                  center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                  zoom: 16,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+ 
+                this.map = new google.maps.Map(document.getElementById("map_canvas"), options);
+            },
+ 
+            (error) => {
+                console.log(error);
+            }, locationOptions
+        );
+    }   
+}
